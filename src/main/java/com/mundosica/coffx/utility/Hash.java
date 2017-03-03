91c3e594192e3277ff3575b57a65137699d235f2
Fitorec <chanerec@gmail.com>
2017-03-03 03:25:11 -0600

    Agregando clase Hash

---
+++ src/main/java/com/mundosica/coffx/utility/Hash.java
@@ -0,0 +1,61 @@
+package com.mundosica.coffx.utility;
+
+/**
+ * Convert Strings to Hashes
+ *
+ * @author fitorec
+ * @url  https://github.com/fitorec/java-hashes
+ * @url https://docs.oracle.com/javase/8/docs/api/java/security/MessageDigest.html
+ */
+public final class Hash {
+        private Hash() {
+            // not instantiable
+        }
+	/**
+	 * Get a hash by txt and hashType
+	 * 
+	 * @param txt, text in plain format
+	 * @param hashType MD5 OR SHA1, etc..
+	 * Note: check list of algorithms
+	 *    http://docs.oracle.com/javase/8/docs/technotes/guides/security/StandardNames.html#MessageDigest
+	 * 
+	 * @return hash in hashType
+	 */
+	public static String getHash(String txt, String hashType) {
+            try {
+	        java.security.MessageDigest md = java.security.MessageDigest.getInstance(hashType);
+	        byte[] array = md.digest(txt.getBytes());
+	        StringBuilder sb = new StringBuilder();
+	        for (int i = 0; i < array.length; ++i) {
+	          sb.append(Integer.toHexString((array[i] & 0xFF) | 0x100).substring(1,3));
+	       }
+	        return sb.toString();
+	    } catch (java.security.NoSuchAlgorithmException e) {
+	    	//error action
+                e.printStackTrace();
+	    }
+	    return null;
+	}
+
+	public static String md2(String txt) {
+		return Hash.getHash(txt, "MD2");
+	}
+	public static String md5(String txt) {
+		return Hash.getHash(txt, "MD5");
+	}
+	public static String sha1(String txt) {
+		return Hash.getHash(txt, "SHA1");
+	}
+	public static String sha224(String txt) {
+		return Hash.getHash(txt, "SHA-224");
+	}
+	public static String sha256(String txt) {
+		return Hash.getHash(txt, "SHA-256");
+	}
+	public static String sha384(String txt) {
+		return Hash.getHash(txt, "SHA-384");
+	}
+	public static String sha512(String txt) {
+		return Hash.getHash(txt, "SHA-512");
+	}
+}
