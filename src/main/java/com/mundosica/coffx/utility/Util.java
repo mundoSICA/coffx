3f721ef8dde20b728a871c3e8166e27a6fbc3b28
Fitorec <chanerec@gmail.com>
2017-03-03 02:59:24 -0600

    Agregando la primera clase Util en el proyecto

---
+++ src/main/java/com/mundosica/coffx/utility/Util.java
@@ -0,0 +1,170 @@
+
+package com.mundosica.coffx.utility;
+
+import java.util.*;
+
+/**
+ * Clase Util se encarga de utilerias en gral.
+ * 
+ * @since 0.1
+ * @author fitorec: Miguel Marcial 
+ * @author Edwin Snow
+ */
+public final class Util {
+
+    private Util() {
+        // not instantiable
+    }
+
+    /**
+     * Regresa true si el arreglo esta vacio
+     *
+     * @since 0.1
+     * @param array arreglo a revisar
+     * @return true si el arreglo esta vacio, false en otro caso
+     */
+    public static boolean empty(Object[] array) {
+        return array == null || array.length == 0;
+    }
+
+    /**
+     * Regresa true si una colección esta vacia
+     *
+     * @since 0.1
+     * @param collection colección a revisar
+     * @return true si la colección esta vacia, false en otro caso
+     */
+    public static boolean empty(Collection<?> collection) {
+        return collection == null || collection.isEmpty();
+    }
+
+    /**
+     * Joins Une con un delimitador a los elementos de un arreglo un String
+     *
+     * @since 0.1
+     * @param array arreglo a unir
+     * @param delimiter delimitador a usar
+     * @return str contiene el resultado del join
+     */
+    public static String join(String[] array, String delimiter) {
+        if (empty(array)) {
+            return "";
+        }
+        String str ="";
+        str += array[0];
+        for (int i = 1; i < array.length; i++) {
+            str += delimiter;
+            str += array[i];
+        }
+        return str;
+    }
+
+    /**
+     * Joins Une con un delimitador a los elementos de un arreglo una colección
+     *
+     * @since 0.1
+     * @param collection colección a unir
+     * @param delimiter delimitador a usar
+     * @return str contiene el resultado del join
+     */
+    public static String join(Collection<?> collection, String delimiter) {
+        if (empty(collection)) {
+            return "";
+        }
+        Iterator<?> it = collection.iterator();
+        String str = "" + it.next();
+        while (it.hasNext()) {
+            str += delimiter;
+            str += it.next();
+        }
+        return str;
+    }
+
+    /**
+     * Repite un texto de entrada un numero de veces indicadas.
+     *
+     * @param in texto de entrada a repetir
+     * @param count El número de veces que se va a repetir in
+     * @return str la cadena resultado.
+     */
+    public static String repeat(String in, int count) {
+        String str = "";
+        for (int i = 0; i < count; i++) {
+            str += in;
+        }
+        return str;
+    }
+
+    /**
+     * Repite un texto de entrada un numero de veces indicadas en un arreglo.
+     *
+     * @param in texto de entrada a repetir
+     * @param count El número de veces que se va a repetir in
+     * @return strArray un arreglo con el resultado.
+     */
+    public static String[] repeatInArray(String in, int count) {
+        if (count<1) {
+            return null;
+        }
+        String strArray[] = new String[count];
+        for (int i = 0; i < count; i++) {
+            strArray[i] = in;
+        }
+        return strArray;
+    }
+
+    /**
+     * Repite un str concatenado de un delimitador count veces
+     *
+     * <p>Ejemplo: <tt>joinAndRepeat("?", ",", 3)</tt> Nos genera <tt>"?,?,?"</tt>
+     *
+     * @param str El string a repetir
+     * @param delimiter El delimitador
+     * @param count El numero de veces a repetir
+     * @return El String generado.
+     */
+    public static String joinAndRepeat(String str, String delimiter, int count) {
+        if(str == null || delimiter == null) {
+            throw new NullPointerException("str and delimiter cannot be null");
+        }
+        if(count<1){
+            throw new IllegalArgumentException("The count must be greater than 5");
+        }
+        return repeat(str+delimiter, count -1)+str;
+    }
+
+    public static Map<String, String> args(String ...input){
+        if( input==null || input.length%2 == 1){
+            throw new NullPointerException("input cannot be null");
+        }
+        Map<String, String> map = new HashMap();
+        for (int i = 0; i < input.length-1; i++) {
+            map.put(input[i], input[i+1]);
+        }
+        return map;
+    }
+    /**
+     * Funciones echo, imprimen en la salida estandar.
+     * 
+     * @param map 
+     */
+    public static void echo(Map map){
+        map.forEach((k,v)->System.out.println(k + " : " + v));
+    }
+    public static void echo(Collection<?> collection) {
+        collection.forEach((v)->System.out.println(v));
+    }
+    public static void echo(String str) {
+        System.out.println(str);
+    }
+    public static void echo(Object o) {
+        System.out.println(o.toString());
+    }
+
+    public static void main(String arg[]){
+        Map<String, String> as = args(
+                "nombre","valor"
+        );
+        echo(as);
+    }
+}
